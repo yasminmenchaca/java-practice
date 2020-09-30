5c7e6c1e2e0fd3b48884743bc7ee23a97b07222c
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-30 13:41:19 -0500

    completed

---
+++ src/IfElse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IfElse {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else if ((N % 2 == 0) && N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if ((N % 2 == 0) && N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
